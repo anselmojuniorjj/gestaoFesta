e7a6ad5c6263a83b4ba83595604317ea7a8f3a68
Anselmo Junior <anselmojuniorjj@gmail.com>
2018-09-03 11:18:08 -0300

    verificação usuário/senha

--- src/main/java/com/teste/gestaofesta/config/inMemorySecurityConfig.java
+++ src/main/java/com/teste/gestaofesta/config/inMemorySecurityConfig.java
@@ -2,18 +2,19 @@ package com.teste.gestaofesta.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class inMemorySecurityConfig {
-/*
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception{
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
 
         builder
-                .inMemoryAuthentication()
-                .withUser("anselmo").password("123456").roles("USER")
-                .and().withUser("junior").password("123456").roles("USER")
-                .and().withUser("joao").password("123456").roles("USER");
-    } */
+                .inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("anselmo").password("123").roles("USER")
+                .and().withUser("junior").password("123").roles("USER")
+                .and().withUser("joao").password("123").roles("USER");
+    }
 }
